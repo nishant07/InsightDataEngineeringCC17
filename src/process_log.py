70cc004587c463b577fb6c8324d69080f187ff69
Nishant Shah <shah.nishant92@gmail.com>
2017-04-02 18:06:47 -0400

    feature 1 and 2 in raw format

--- src/process_log.py
+++ src/process_log.py
@@ -17,19 +17,28 @@ def top_k_elements(element_dict, top_k=[], k=10):
 			list: Return a list with k elements
 
 	"""
+	#print element_dict
 	element = element_dict.items() 
 	element_key, element_value = element[0][0], element[0][1]
 	
-	if top_k or len(top_k) < k:
+	if top_k:
+		element_keys = map(lambda k: k[1], top_k)
+		#print element_keys
+
+	if top_k and element_key in element_keys:
+		del top_k[element_keys.index(element_key)]
 		heappush(top_k, (element_value, element_key))
-	else:
-		if top_k[0][0] == element_value:
-			if top_k[0][1] > element_key:
-				heapreplace(top_k, (element_value, element_key))
-		elif top_k[0] < (element_value, element_key):
-			heapreplace(top_k, (element_value, element_key))
+	else:		
+		if not top_k or len(top_k) < k:
+			heappush(top_k, (element_value, element_key))
 		else:
-			pass
+			if top_k[0][0] == element_value:
+				if top_k[0][1] > element_key:
+					heapreplace(top_k, (element_value, element_key))
+			elif top_k[0] < (element_value, element_key):
+				heapreplace(top_k, (element_value, element_key))
+			else:
+				pass
 	return top_k
 
 def decompose_server_log(server_log):
@@ -42,17 +51,20 @@ def decompose_server_log(server_log):
 		tuple: A tuple with decomposed values mentioned in the definition
 	"""
 	raw_log = server_log.split()
-	host = raw_log[0]
-	timestamp = re.search(r"\[(.*)\]",server_log).group()
-	request = re.search(r"\"(.*)\"", server_log).group()
-	resource = request.split()[1]
-	response_code = raw_log[-2]
-	if raw_log[-1] == '-':
-		bytes = 0
-	else:
-		bytes = int(raw_log[-1])
-	print server_log
-	print host, timestamp, request, resource, response_code, bytes
+	try:
+		host = raw_log[0]
+		timestamp = re.search(r"\[(.*)\]",server_log).group()
+		request = re.search(r"\"(.*)\"", server_log).group()
+		resource = request.split()[1]
+		response_code = raw_log[-2]
+		if raw_log[-1] == '-':
+			bytes = 0
+		else:
+			bytes = int(raw_log[-1])
+	except IndexError:
+		print raw_log
+		return ('dummy_host','dummy_ts','SOS',-1)
+	return (host, timestamp, resource, response_code, bytes)
 
 def analyze_server_logs():
 	""" Main function from which the execution begins for
@@ -60,12 +72,31 @@ def analyze_server_logs():
 	"""
 	host_list = Counter()
 	timestamps = list()
-	resources_list = Counter()
+	resources_list = dict()
+	top_k_host = []
+	top_k_resources = []
 	with open(argv[1],'r') as input_file:		
 		for server_log in input_file:
-			print server_log
-			decompose_server_log(server_log)
-			break
+			decomposed_log = decompose_server_log(server_log)
+			#print decomposed_log
+			try:
+				host = decomposed_log[0]
+				resource = decomposed_log[2]
+				bytes = decomposed_log[4]
+			except IndexError:
+				print "Host,resource,bytes", decomposed_log
+				continue
+
+			host_list[host] += 1
+			resources_list[resource] = resources_list.get(resource,0) + bytes
+
+			top_k_host = top_k_elements({host: host_list[host]}, top_k_host)
+			top_k_resources = top_k_elements({resource: resources_list[resource]}, top_k_resources)
+
+	print top_k_host
+	print top_k_resources
+
+			
 			
 if __name__ == "__main__":
 	analyze_server_logs()
